62c803495f41010bfa3ffbc781623c524e678ea8
hansonryne <hansonryne@gmail.com>
2018-02-20 23:49:51 -0500

    Add links to old scans in navbar. Adjust logo size in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :make_nmapscans_variable
+
+  def make_nmapscans_variable
+   @nmapscans = Nmapscan.all 
+  end
+  
 end
